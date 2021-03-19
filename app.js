07b2831eb26613449c342d92a8e7f1fdca6afd21
Dohyeon Han <limited.hyeon@gmail.com>
2021-03-20 01:48:50 +0900

    Drawing

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const canvas = document.getElementById("jsCanvas"),
     colors = document.getElementsByClassName("controls_color"),
     range = document.querySelector(".controls_range"),
     mode = document.getElementById("jsMode"),
-    saveBtn = document.getElementById("jsSave"),
-    lineBtn = document.getElementById("jsLine");
+    saveBtn = document.getElementById("jsSave");
 
 canvas.width = 630;
 canvas.height = 540;
@@ -14,10 +13,7 @@ ctx.lineWidth = 4;
 ctx.fillStyle = "white";
 
 let painting = false,
-    filling = false,
-    line = false,
-    startX=0,startY=0,
-    existingLines = [];
+    filling = false;
 
 
 function handleModeClick(event){
@@ -38,15 +34,15 @@ function startPainting(){
 
 function stopPainting(){
     painting = false;
+    console.log(existingLines);
 }
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    //ctx.fillRect(0,0,640,550)
- 
-    if(filling ===false){
-        if(line===false){
+    
+    
+    if(filling===false){
             if(!painting){
                  ctx.beginPath();//시작위치
                  ctx.moveTo(x,y);
@@ -56,29 +52,8 @@ function onMouseMove(event){
             }
             
         }
-        // else{
-        //     if(!painting){
-        //         startX = x;
-        //         startY = y;
-        //     }else{
-                
-        //         ctx.beginPath();//시작위치
-        //         ctx.moveTo(startX,startY);
-        //         ctx.lineTo(x,y);//이전 위치부터 현재까지 선을 이음
-        //         ctx.stroke()
-        //         existingLines.push({
-        //             startX,
-        //             startY,
-        //             endX:x,
-        //             endY:y,
-        //             color:ctx.strokeStyle,
-        //             width:ctx.lineWidth
-        //         })
-        //     }
-        // }
-    }
-    
 }
+
 
 function changeColor(event){
     if(filling ===false){
@@ -133,7 +108,4 @@ if(mode){
 }
 if(saveBtn){
     saveBtn.addEventListener("click",handleSaveClick);
-}
-if(lineBtn){
-    lineBtn.addEventListener("click",handleLineClick);
 }
